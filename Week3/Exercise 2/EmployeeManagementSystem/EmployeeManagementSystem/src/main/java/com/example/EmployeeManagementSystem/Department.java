52446cbf1725124a1a461c3dd5f800c08be2fc12
pro-jesh <lionelprajesh@gmail.com>
2024-08-13 14:22:56 +0530

    first commit

---
+++ Week3/Exercise 2/EmployeeManagementSystem/EmployeeManagementSystem/src/main/java/com/example/EmployeeManagementSystem/Department.java
@@ -0,0 +1,26 @@
+package com.example.EmployeeManagementSystem;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Table(name = "departments")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Department {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "department")
+    private List<Employee> employees;
+}
+
---
+++ Week3/Exercise 3/EmployeeManagementSystem/EmployeeManagementSystem/src/main/java/com/example/EmployeeManagementSystem/repository/EmployeeRepository.java
@@ -0,0 +1,13 @@
+package com.example.EmployeeManagementSystem.repository;
+
+import com.example.EmployeeManagementSystem.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+    List<Employee> findByName(String name);
+    List<Employee> findByDepartmentName(String departmentName);
+}
